09988168cb21775fa8b7e79edaabec0bf79b1074
AlxMen <alejandrocs402@gmail.com>
2024-03-20 13:10:18 +0000

    Proyecto terminado

---
+++ src/components/OrderContents.tsx
@@ -0,0 +1,41 @@
+import { formatCurrency } from "../helpers";
+import { MenuItem, OderItem } from "../types"
+
+type OrderContentsProps = {
+  order: OderItem[],
+  removeItem: (id: MenuItem['id']) => void
+};
+
+
+export default function OrderContents({ order, removeItem }: OrderContentsProps) {
+  return (
+    <div>
+      <h2 className="font-black text-4xl">Pedido</h2>
+
+      <div className="space-y-3 mt-10">
+        {order.map((item) => (
+            <div
+              key={item.id}
+              className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b "
+            >
+              <div>
+                <p className="text-lg">
+                  {item.name} - {formatCurrency(item.price)}
+                </p>
+                <p className="font-black">
+                  Cantidad: {item.quantity} -{" "}
+                  {formatCurrency(item.price * item.quantity)}
+                </p>
+              </div>
+              <button
+                className="bg-red-600 hover:bg-red-800 h-8 w-8 rounded-full text-white font-black"
+                onClick={() => removeItem(item.id)}
+              >
+                X
+              </button>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
